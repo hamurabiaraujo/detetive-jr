647fae60f28f13d67e29298e66f6fff9b7a6f19f
Hamurabi Araújo <araujohamurabi@gmail.com>
2021-01-22 19:43:03 -0300

    Fix sorteio dos Vilões

--- src/app/services/locais/locais.service.ts
+++ src/app/services/locais/locais.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Local } from 'src/app/interfaces/local';
+import { Vilao } from 'src/app/interfaces/vilao';
 import { EnvelopeService } from '../envelope/envelope.service';
 import { ViloesService } from '../viloes/viloes.service';
 
@@ -66,22 +67,43 @@ export class LocaisService {
     return this.locais.find( local => id === local.id );
   }
 
+  // https://stackoverflow.com/a/2450976/5636215
+  private shuffle(array: Vilao[]) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+  
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+  
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+  
+    return array;
+  }
+
   sortearViloes() {
     const viloes = this.viloesService.get();
-    viloes.sort(() => Math.random() - 0.5);
-    
-    console.log(viloes);
+    const viloesSorteados = this.shuffle( [...viloes] );
 
     // preenchendo os locais com os vilões embaralhados
     const locaisComViloes = this.locais.map( (local, index) => {
-      local.vilao = viloes[index];
+      local.vilao = viloesSorteados[index];
       return local;
     });
 
     this.locais = locaisComViloes;
 
     const quantidadeViloes = viloes.length;
+    
     //atualizando o evelope com os 2 últimos vilões embaralhados
-    this.envelopeService.viloes = [viloes[quantidadeViloes - 1], viloes[quantidadeViloes - 2]];
+    this.envelopeService.viloes = [
+      viloesSorteados[quantidadeViloes - 2], 
+      viloesSorteados[quantidadeViloes - 1]
+    ];
   }
 }
